10a236634f332464b4a0e21e8165298723b61fee
zotel1 <zotelsigel@gmail.com>
2024-07-16 00:59:45 -0300

    update: agregamos el audio.d.ts para poder usar archivos .wav

--- next.config.mjs
+++ next.config.mjs
@@ -19,8 +19,7 @@ const nextConfig = {
           loader: "url-loader",
           options: {
             limit: config.inlineImageLimit,
-            fallback: "file-loader",
-            publicPath: `${config.assetPrefix}/_next/static/images/`,
+            publicPath: `/_next/static/images/`,
             outputPath: `${isServer ? "../" : ""}static/images/`,
             name: "[name]-[hash].[ext]",
             esModule: config.esModule || false,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import TRANSCRIPT from "../app/data/transcript.json";
+import AUDIO from "../app/data/audio.wav";
 
 export interface Message {
   content: string;
@@ -9,15 +10,18 @@ export interface Message {
 
 export default function HomePage() {
   return (
-    <ul className="grid gap-4">
-      {TRANSCRIPT.map((message) => (
-        <li
-          key={message.start}
-          className={`rounded bg-neutral-900 p-4 ${message.role === "user" ? "justify-self-end bg-neutral-700" : "bg-neutral-800"}`}
-        >
-          {message.content}
-        </li>
-      ))}
-    </ul>
+    <section className="grid gap-4">
+      <ul className="grid gap-4">
+        {TRANSCRIPT.map((message) => (
+          <li
+            key={message.start}
+            className={`rounded bg-neutral-900 p-4 ${message.role === "user" ? "justify-self-end bg-neutral-700" : "bg-neutral-800"}`}
+          >
+            {message.content}
+          </li>
+        ))}
+      </ul>
+      <audio controls className="w-full" src={AUDIO} />
+    </section>
   );
 }
